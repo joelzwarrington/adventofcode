0eccf7b1237af2f71093919e394edf094428249e
Joel Warrington <joelw@hey.com>
2023-12-06 14:46:39 -0700

    Solve 2023 06 (#6)

---
+++ challenges/2023/06/solution.rb
@@ -0,0 +1,51 @@
+# frozen_string_literal: true
+
+module Year2023
+  class Day06 < Solution
+    def part_1
+      times = data.first.scan(/\d+/).map(&:to_i)
+      distances = data.second.scan(/\d+/).map(&:to_i)
+      number_of_races = times.size
+
+      (0...number_of_races).reduce(0) do |total, race|
+        time = times[race]
+        distance_to_beat = distances[race]
+
+        leftmost = 0.upto(time).find do |seconds_holding_button|
+          distance = seconds_holding_button * (time - seconds_holding_button)
+          distance > distance_to_beat
+        end
+
+        rightmost = time.downto(0).find do |seconds_holding_button|
+          distance = seconds_holding_button * (time - seconds_holding_button)
+          distance > distance_to_beat
+        end
+
+        chances_to_win = rightmost - leftmost + 1
+
+        if total.zero?
+          chances_to_win
+        else
+          total * chances_to_win
+        end
+      end
+    end
+
+    def part_2
+      time = data.first.scan(/\d/).join.to_i
+      distance_to_beat = data.second.scan(/\d/).join.to_i
+
+      leftmost = 0.upto(time).find do |seconds_holding_button|
+        distance = seconds_holding_button * (time - seconds_holding_button)
+        distance > distance_to_beat
+      end
+
+      rightmost = time.downto(0).find do |seconds_holding_button|
+        distance = seconds_holding_button * (time - seconds_holding_button)
+        distance > distance_to_beat
+      end
+
+      rightmost - leftmost + 1
+    end
+  end
+end
---
+++ spec/2023/06/solution_spec.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+RSpec.describe Year2023::Day06 do
+  let(:input) do
+    <<~EOF
+      Time:      7  15   30
+      Distance:  9  40  200
+    EOF
+  end
+
+  describe "part 1" do
+    subject { described_class.part_1(input) }
+
+    it { is_expected.to eq(288) }
+  end
+
+  describe "part 2" do
+    subject { described_class.part_2(input) }
+
+    it { is_expected.to eq(71503) }
+  end
+end
