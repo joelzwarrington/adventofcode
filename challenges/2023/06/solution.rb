8b7f6fa570d3ba370f1c28a5f5b50ad914da825e
Joel Warrington <joel.warrington@shopify.com>
2023-12-05 23:01:29 -0700

    Solve part 1

--- challenges/2023/06/solution.rb
+++ challenges/2023/06/solution.rb
@@ -1,11 +1,33 @@
 # frozen_string_literal: true
 module Year2023
   class Day06 < Solution
-    # @input is available if you need the raw data input
-    # Call `data` to access either an array of the parsed data, or a single record for a 1-line input file
-
     def part_1
-      nil
+      times = data.first.scan(/\d+/).map(&:to_i)
+      distances = data.second.scan(/\d+/).map(&:to_i)
+      number_of_races = times.size
+
+      (0...number_of_races).reduce(0) do |total, race|
+        time = times[race]
+        distance_to_beat = distances[race]
+
+        leftmost = 0.upto(time).find do |seconds_holding_button|
+          distance = seconds_holding_button * (time - seconds_holding_button)
+          distance > distance_to_beat
+        end
+
+        rightmost = time.downto(0).find do |seconds_holding_button|
+          distance = seconds_holding_button * (time - seconds_holding_button)
+          distance > distance_to_beat
+        end
+
+        chances_to_win = rightmost - leftmost + 1
+
+        if total.zero?
+          chances_to_win
+        else
+          total * chances_to_win
+        end
+      end
     end
 
     def part_2
--- spec/2023/06/solution_spec.rb
+++ spec/2023/06/solution_spec.rb
@@ -2,31 +2,26 @@
 require 'spec_helper'
 
 RSpec.describe Year2023::Day06 do
-  let(:input) { File.read(File.join(File.dirname(__FILE__), "../../../challenges/2023/06/input.txt")) }
-  let(:example_input) {
+  let(:input) {
     <<~EOF
-      change
-      me
+      Time:      7  15   30
+      Distance:  9  40  200
     EOF
   }
 
   describe "part 1" do
-    it "returns nil for the example input" do
-      expect(described_class.part_1(example_input)).to eq(nil)
-    end
+    subject { described_class.part_1(input) }
 
-    it "returns nil for my input" do
-      expect(described_class.part_1(input)).to eq(nil)
-    end
+    it { is_expected.to eq(288) }
   end
 
   describe "part 2" do
-    it "returns nil for the example input" do
-      expect(described_class.part_2(example_input)).to eq(nil)
-    end
+    # it "returns nil for the example input" do
+    #   expect(described_class.part_2(example_input)).to eq(nil)
+    # end
 
-    it "returns nil for my input" do
-      expect(described_class.part_2(input)).to eq(nil)
-    end
+    # it "returns nil for my input" do
+    #   expect(described_class.part_2(input)).to eq(nil)
+    # end
   end
 end
