200b2197841ea1fbb875599ae8ee830a4ae18567
Joel Warrington <joel.warrington@shopify.com>
2023-12-07 23:25:07 -0700

    Refactored solutions

--- challenges/2023/08/solution.rb
+++ challenges/2023/08/solution.rb
@@ -3,73 +3,82 @@
 module Year2023
   class Day08 < Solution
     class Node
-      attr_reader :id, :left, :right
+      attr_accessor :id
 
-      def initialize(id, left, right)
+      def initialize(network, id, left_id, right_id)
+        @network = network
         @id = id
-        @left = left
-        @right = right
+        @left_id = left_id
+        @right_id = right_id
       end
+
+      def left
+        @network.nodes[@left_id]
+      end
+
+      def right
+        @network.nodes[@right_id]
+      end
+
+      def inspect
+        "<Node id=\"#{id}\">"
+      end
+    end
+
+    class Network
+      attr_accessor :nodes
+
+      def initialize(nodes = nil)
+        @nodes = nodes
+      end
+    end
+
+    def network
+      @network ||= begin
+        network = Network.new
+
+        nodes = data.from(2).each_with_object({}) do |line, object|
+          id, left, right = line.scan(/\w+/)
+          object[id] = Node.new(network, id, left, right)
+        end
+
+        network.nodes = nodes
+        network
+      end
+    end
+
+    def directions
+      @directions ||= data.first.scan(/\w/).map { |direction| direction == "L" ? :left : :right }
+    end
+
+    def direction_given_step(step)
+      directions[(step - 1) % directions.size]
     end
 
     def part_1
-      directions = data.first.scan(/\w/).map { |direction| direction == "L" ? :left : :right }
-      nodes = data.from(2).each_with_object({}) do |line, object|
-        id, left, right = line.scan(/\w+/)
-        object[id] = Node.new(id, left, right)
+      node = network.nodes["AAA"]
+
+      (1..).find do |step|
+        node = node.send(direction_given_step(step))
+        node.id == "ZZZ"
       end
-
-      steps = 0
-      found = false
-      current_node = nodes["AAA"]
-
-      until found
-        direction = directions[steps % directions.size]
-
-        node_id = current_node.public_send(direction)
-        steps += 1
-
-        if node_id == "ZZZ"
-          found = true
-        else
-          current_node = nodes[node_id]
-        end
-      end
-
-      steps
     end
 
     def part_2
-      directions = data.first.scan(/\w/).map { |direction| direction == "L" ? :left : :right }
-      nodes_map = data.from(2).each_with_object({}) do |line, object|
-        id, left, right = line.scan(/\w+/)
-        object[id] = Node.new(id, left, right)
-      end
+      nodes = network.nodes.values.filter { |node| node.id.ends_with?("A") }
 
-      nodes = nodes_map.values.filter_map { |node| node if node.id.ends_with?("A") }
-      steps_per_node = Array.new(nodes.size, 0)
-
-      nodes.each_with_index do |node, index|
-        steps = 0
-        found = false
-        current_node = node
-
-        until found
-          direction = directions[steps % directions.size]
-          node_id = current_node.public_send(direction)
-
-          steps += 1
-          if node_id.ends_with?("Z")
-            found = true
-          else
-            current_node = nodes_map[node_id]
-          end
+      nodes.reduce(0) do |total_steps, node|
+        steps = (1..).find do |step|
+          node = node.send(direction_given_step(step))
+          node.id.ends_with?("Z")
         end
 
-        steps_per_node[index] = steps
+        if total_steps.zero?
+          steps
+        else
+          total_steps.lcm(steps)
+        end
       end
-
-      steps_per_node.reduce(&:lcm)
     end
   end
 end
