641a37a736d332e9b839b1a93e5200407932c2b9
Joel Warrington <joel.warrington@shopify.com>
2023-12-08 22:50:00 -0700

    Solve part 1

--- challenges/2023/09/solution.rb
+++ challenges/2023/09/solution.rb
@@ -2,8 +2,30 @@
 
 module Year2023
   class Day09 < Solution
+    class Sequence
+      attr_reader :numbers, :child
+
+      def initialize(numbers)
+        @numbers = numbers
+        unless numbers.all?(&:zero?) || numbers.size == 1
+          @child = Sequence.new(
+            (0...(numbers.size - 1)).map { |index| numbers[index + 1] - numbers[index] },
+          )
+        end
+      end
+
+      def next_value
+        if @child.present?
+          @numbers.last + @child.next_value
+        else
+          0
+        end
+      end
+    end
+
     def part_1
-      nil
+      sequences = data.map { |line| Sequence.new(line.scan(/-?\d+/).map(&:to_i)) }
+      sequences.sum(&:next_value)
     end
 
     def part_2
