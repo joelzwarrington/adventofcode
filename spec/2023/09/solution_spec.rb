460e7984935cd608964ee2cf8a7286584f183948
Joel Warrington <joel.warrington@shopify.com>
2023-12-08 22:13:10 -0700

    Setup

--- spec/2023/09/solution_spec.rb
+++ spec/2023/09/solution_spec.rb
@@ -1,32 +1,25 @@
 # frozen_string_literal: true
-require 'spec_helper'
+
+require "spec_helper"
 
 RSpec.describe Year2023::Day09 do
-  let(:input) { File.read(File.join(File.dirname(__FILE__), "../../../challenges/2023/09/input.txt")) }
-  let(:example_input) {
+  let(:input) do
     <<~EOF
-      change
-      me
+      0 3 6 9 12 15
+      1 3 6 10 15 21
+      10 13 16 21 30 45
     EOF
-  }
+  end
 
   describe "part 1" do
-    it "returns nil for the example input" do
-      expect(described_class.part_1(example_input)).to eq(nil)
-    end
+    subject { described_class.part_1(input) }
 
-    it "returns nil for my input" do
-      expect(described_class.part_1(input)).to eq(nil)
-    end
+    it { is_expected.to eq(114) }
   end
 
   describe "part 2" do
-    it "returns nil for the example input" do
-      expect(described_class.part_2(example_input)).to eq(nil)
-    end
+    subject { described_class.part_2(input) }
 
-    it "returns nil for my input" do
-      expect(described_class.part_2(input)).to eq(nil)
-    end
+    it.skip { is_expected.to eq(nil) }
   end
 end
